c6dcd726c7deca3a3329e2d5d6b9117b8c2d94fd
Felix Gravila <felixgravila@gmail.com>
2025-02-27 14:16:04 +0100

    Most of the functionality implemented

--- esp32_pomodoro.ino
+++ esp32_pomodoro.ino
@@ -1,6 +1,10 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
+#define REFRESH_RATE_MS 10
+#define SPEEDUP_FACTOR 100  // For debugging
+#define PULSE_PERIOD_MS 5000
+
 #define CLK 16
 #define DT 17
 #define SW 15
@@ -11,13 +15,15 @@
 #define BRIGHTNESS 100
 #define LED_TYPE WS2811
 #define COLOR_ORDER GRB
+CRGB abstracted_leds[NUM_LEDS];
 CRGB leds[NUM_LEDS];
-int using_red = 1;
 
+bool paused = false;                 // if time is currently stopped;
+bool useLongFormBreak = false;       // If green should span 360 degrees
+uint32_t current_set_time_s = 0;     // 0-1499 work, 1500-1799 pause
+uint32_t pulse_time_counter_ms = 0;  // safer than millis() % PULSE_PERIOD_MS
 
-volatile int16_t inputDelta = 0;  // Updated inside ISR
-volatile bool eventFlag = false;  // Indicates if new data is available
-
+volatile int16_t inputDelta = 0;          // Amount rotenc got turned
 volatile bool clickFlag = false;          // Single click detected
 volatile bool longPressFlag = false;      // Long press detected
 volatile bool veryLongPressFlag = false;  // Very long press detected
@@ -28,7 +34,14 @@ int16_t inputDelta_copy = 0;
 bool clickFlag_copy = false;
 bool longPressFlag_copy = false;
 bool veryLongPressFlag_copy = false;
-uint8_t inputDelta_remainder = 0;
+
+void abstract_leds() {
+  // Simplify model assuming idx 0 = minute 1 on clock face
+  for (int i = 0; i < NUM_LEDS; i++) {
+    abstracted_leds[(i + 24) % NUM_LEDS] = leds[i];
+  }
+}
+
 
 void IRAM_ATTR encoderISR() {
   static uint8_t lastState = 0;
@@ -42,7 +55,6 @@ void IRAM_ATTR encoderISR() {
   }
 
   lastState = state;
-  eventFlag = true;
 }
 
 void IRAM_ATTR buttonISR() {
@@ -68,6 +80,23 @@ void IRAM_ATTR buttonISR() {
   lastInterruptTime = currentTime;
 }
 
+void setLedValueForTime(int time_s, uint8_t red, uint8_t green, uint8_t blue, float brightness) {
+  time_s = constrain(time_s, 0, 1499);
+  brightness = constrain(brightness, 0, 1);
+  float threshold = (time_s / 1499.0) * NUM_LEDS;
+  int fullOffCount = floor(threshold);          // Completely off LEDs
+  float fade = 1 - (threshold - fullOffCount);  // Fractional part for transition
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if (i < fullOffCount) {
+      leds[i].setRGB(0, 0, 0);
+    } else if (i == fullOffCount) {
+      leds[i].setRGB(red * brightness * fade, green * brightness * fade, blue * brightness * fade);
+    } else {
+      leds[i].setRGB(red * brightness, green * brightness, blue * brightness);
+    }
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -75,7 +104,7 @@ void setup() {
   pinMode(DT, INPUT_PULLUP);
   pinMode(SW, INPUT_PULLUP);
 
-  FastLED.addLeds<WS2812B, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812B, LED_PIN, COLOR_ORDER>(abstracted_leds, NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
   Serial.println("Hello");
 
@@ -84,8 +113,8 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(SW), buttonISR, CHANGE);
 }
 
-int i = 0;
 void loop() {
+  // Read volatile flags
   noInterrupts();
   clickFlag_copy = clickFlag;
   longPressFlag_copy = longPressFlag;
@@ -93,45 +122,52 @@ void loop() {
   clickFlag = false;
   longPressFlag = false;
   veryLongPressFlag = false;
+
+  inputDelta_copy = inputDelta;
+  inputDelta = 0;
   interrupts();
 
   if (clickFlag_copy) {
-    Serial.println("Click!");
-    using_red = 1 - using_red;
+    Serial.println("Clicked!");
+    paused = !paused;
   }
+
   if (longPressFlag_copy) {
     Serial.println("Long click!");
-  }
-  if (veryLongPressFlag_copy) {
-    Serial.println("Very Long click!");
+    useLongFormBreak = !useLongFormBreak;
   }
 
-  if (eventFlag || clickFlag_copy || longPressFlag_copy || veryLongPressFlag_copy) {
-    eventFlag = false;
+  // Handle pulsing
+  float pulseModifier = 1.0;
+  if (paused) {
+    float pulseModifier0to1 = (1 - cos(pulse_time_counter_ms * PI * 2 / PULSE_PERIOD_MS)) / 2;
+    pulseModifier = 0.1 + pulseModifier0to1 * 0.9;
+  }
 
-    noInterrupts();
-    inputDelta = constrain(inputDelta, 0, NUM_LEDS * 4 - 1);
-    inputDelta_copy = inputDelta;
-    interrupts();
+  // Handle manual time editing if paused
+  if (paused) {
+    current_set_time_s = (current_set_time_s + inputDelta_copy * 10) % 1800;
+  }
 
-    inputDelta_remainder = inputDelta_copy % 4;
-    inputDelta_copy = std::round(inputDelta_copy / 4);
-    Serial.println(inputDelta_copy);
-
-    // leds[i] = 0xFF44DD;
-    // leds[i].setRGB( 255, 68, 221);
-
-    for (int i = 0; i < inputDelta_copy; i++) {
-      leds[(i + 25) % NUM_LEDS].setRGB(255 * using_red, 255 * (1 - using_red), 0);
+  if (current_set_time_s < 1500) {
+    // working
+    setLedValueForTime(current_set_time_s, 255, 0, 0, pulseModifier);
+  } else {
+    int value = current_set_time_s - 300; // 1200-1499
+    if (useLongFormBreak) {
+      value = (value - 1200) * 5;
+      setLedValueForTime(value, 0, 200, 70, pulseModifier);
+    } else {
+      setLedValueForTime(value, 0, 255, 0, pulseModifier);
     }
-
-    // Set fade brightness if transitioning
-    leds[(inputDelta_copy + 25) % NUM_LEDS].setRGB(255 * using_red * inputDelta_remainder / 4, 255 * (1 - using_red) * inputDelta_remainder / 4, 0);
-
-    for (int i = inputDelta_copy + 1; i < NUM_LEDS; i++) {
-      leds[(i + 25) % NUM_LEDS].setRGB(0, 0, 0);
-    }
-    FastLED.show();
   }
-  delay(10);
+  abstract_leds();
+  FastLED.show();
+
+  // Add time if not paused
+  if (!paused) {
+    current_set_time_s = (current_set_time_s + (SPEEDUP_FACTOR * REFRESH_RATE_MS / 1000)) % 1800;
+  }
+  pulse_time_counter_ms = (pulse_time_counter_ms + REFRESH_RATE_MS) % PULSE_PERIOD_MS;
+  delay(REFRESH_RATE_MS);
 }
